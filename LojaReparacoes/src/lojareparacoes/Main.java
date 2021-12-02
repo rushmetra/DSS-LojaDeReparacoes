53501d109d843eb5c0d39b1e5f2428b43a89ad22
Rui Guilherme Monteiro <61871927+rushmetra@users.noreply.github.com>
2021-12-02 21:49:56 +0000

    Update Main.java

--- LojaReparacoes/src/lojareparacoes/Main.java
+++ LojaReparacoes/src/lojareparacoes/Main.java
@@ -1,10 +1,4 @@
-/*
- *  DISCLAIMER: Este código foi criado para discussão e edição durante as aulas práticas de DSS, representando
- *  uma solução em construção. Como tal, não deverá ser visto como uma solução canónica, ou mesmo acabada.
- *  É disponibilizado para auxiliar o processo de estudo. Os alunos são encorajados a testar adequadamente o
- *  código fornecido e a procurar soluções alternativas, à medida que forem adquirindo mais conhecimentos.
- */
-package uminho.dss.turmas3l;
+package lojareparacoes;
 
 /**
  * @author DSS
