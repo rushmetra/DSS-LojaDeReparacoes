a0d27169a5fbff680151db1bb2403b274f46f679
rushmetra <a93179@alunos.uminho.pt>
2021-12-28 20:20:33 +0000

    View

--- src/LRView/IUserInterface.java
+++ src/LRView/IUserInterface.java
@@ -1,4 +1,12 @@
-package LRView;
+package view;
+
+import business.*;
+import java.util.Collection;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public interface IUserInterface {
+
+
 }
