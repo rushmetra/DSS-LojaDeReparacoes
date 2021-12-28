c44ccb0256b6d43f0c354eb35cff6ce1858e8ffc
Rodrigo Rodrigues <55801579+webst2r@users.noreply.github.com>
2021-12-28 18:54:03 +0000

    Passo

---
+++ src/business/Passo.java
@@ -0,0 +1,24 @@
+package business;
+
+public class Passo {
+    private String descricao;
+    private float tempo;
+    private float tempoPrevisto;
+    private float custo;
+    private boolean concluido;
+
+
+    public Passo(float custo,float tempo,float tempoPrevisto,String descricao,boolean concluido){
+        this.custo = custo;
+        this.tempo = tempo;
+        this.tempoPrevisto = tempoPrevisto;
+        this.descricao = descricao;
+        this.concluido = concluido;
+    }
+
+    public String getDescricao() { return this.descricao;}
+    public float getTempo() { return this.tempo;}
+    public float getTempoPrevisto() { return this.tempoPrevisto; }
+    public float getCusto() { return this.custo;}
+    public boolean getConcluido() { return this.concluido; }
+}
