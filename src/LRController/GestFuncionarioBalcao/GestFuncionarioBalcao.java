e1d3de533a84ce72047ba3e9575fccd679043fd9
Rui Filipe Coelho Moreira <61985785+RuiMoreiraA93232@users.noreply.github.com>
2021-12-28 19:41:18 +0000

    algumas funcoes do funcionario balcao

--- src/LRController/GestFuncionarioBalcao/GestFuncionarioBalcao.java
+++ src/LRController/GestFuncionarioBalcao/GestFuncionarioBalcao.java
@@ -1,7 +1,35 @@
 package LRController.GestFuncionarioBalcao;
 
 import LRModel.LojaReparacoesModel;
+import LRModel.PedidoExpresso;
+import LRModel.PedidoOrcamento;
 
 public class GestFuncionarioBalcao {
     LojaReparacoesModel model;
+
+    public void registarPedido(String nomeCliente, String contacto, String nif, String email){
+        PedidoOrcamento po = new PedidoOrcamento(nomeCliente,contacto,email,nif);
+        model.adicionaPedidoOrcamento(po);
+    }
+
+    public void registarServicoExpresso(String nif,String contacto){
+        PedidoExpresso pe = new PedidoExpresso(nif,contacto);
+        model.adicionaPedidoExpresso(pe);
+    }
+
+    public void registarConclusaoPedido(String nif){
+        PedidoOrcamento po = this.model.getPedidoOrcamento(nif);
+        po.setConlusaoPedido(true);
+    }
+
+    public void registarConfirmacaoReparacao(String nif){
+        PedidoOrcamento po = this.model.getPedidoOrcamento(nif);
+        po.setConfirmacaoReparacao(true);
+    }
+
+    // falta pensar o que vai ter as entregas
+    public void registarEntregaEquipamento(String nif){
+
+    }
+
 }
