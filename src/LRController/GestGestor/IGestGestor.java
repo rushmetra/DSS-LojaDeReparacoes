12379b950881f2312cc4f871425627541785757c
jjgonc <josejoao_2001@hotmail.com>
2021-12-28 23:22:35 +0000

    adicionados à Interface

--- src/LRController/GestGestor/IGestGestor.java
+++ src/LRController/GestGestor/IGestGestor.java
@@ -1,5 +1,19 @@
 package LRController.GestGestor;
 
+import LRModel.*;
+
+import java.util.*;
+
 public interface IGestGestor {
     public boolean loginGestor(String username, String password);
+
+    public Gestor getGestor(String id);
+
+    public List<String> getListagem1 ();
+
+    public List<String> getListagem2();
+
+    public List<String> getListagem3();
+
+    public List<String> parseToDisplay (Map<String, AbstractMap.SimpleEntry<List<List<Passo>>, List<String>>> listaIntervencoes);
 }
